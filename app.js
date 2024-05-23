7c8b7b561642c5063e642c90626defa783b33345
Naksikus <166904155+Naksikusik@users.noreply.github.com>
2024-05-24 00:21:30 +0500

    hh

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('./middlewares/cors');
+const path = require('path');
+const apiRouter = require('./routes/api');
+const cookieParser = require("cookie-parser");
+const connectToDataBase = require('./database/connect');
+const pagesRouter = require('./routes/pages');
+const app = express();
+
+const PORT = 3002;
+connectToDataBase();
+app.use(
+    cors,
+    cookieParser(),
+    bodyParser.json(),
+    pagesRouter,
+    apiRouter,
+    express.static(path.join(__dirname, 'public'))
+);
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен http://localhost:${PORT}`)
+}); 
---
+++ routes/users.js
@@ -0,0 +1,52 @@
+const usersRouter = require('express').Router();
+const {
+    findAllUsers,
+    findUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    checkEmptyNameAndEmail,
+    checkEmptyNameAndEmailAndPassword,
+    checkIsUserExists,
+    hashPassword,
+} = require('../middlewares/users');
+const {
+    sendAllUsers,
+    sendUserById,
+    sendMe,
+    sendUserCreated,
+    sendUserUpdated,
+    sendUserDeleted
+} = require('../controllers/users');
+
+const { checkAuth } = require('../middlewares/auth');
+
+usersRouter.get("/me", checkAuth, sendMe);
+usersRouter.get('/users', findAllUsers, sendAllUsers);
+usersRouter.get('/users/:id', findUserById, sendUserById);
+usersRouter.post(
+    '/users',
+    findAllUsers,
+    checkIsUserExists,
+    checkEmptyNameAndEmailAndPassword,
+    checkAuth,
+    hashPassword,
+    createUser,
+    sendUserCreated
+);
+usersRouter.put(
+    '/users/:id',
+    findUserById,
+    checkEmptyNameAndEmail,
+    checkAuth,
+    updateUser,
+    sendUserUpdated
+);
+usersRouter.delete(
+    '/users/:id',
+    checkAuth,
+    deleteUser,
+    sendUserDeleted
+);
+
+module.exports = usersRouter;
